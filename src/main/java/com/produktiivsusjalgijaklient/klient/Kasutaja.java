7c07c1ff46c4e99b4f445f494053d7f693352f71
hanneskolk <145332370+hanneskolk@users.noreply.github.com>
2024-05-14 21:12:23 +0300

    Hannese katsetus

--- src/main/java/com/produktiivsusjalgijaklient/klient/Kasutaja.java
+++ src/main/java/com/produktiivsusjalgijaklient/klient/Kasutaja.java
@@ -3,4 +3,12 @@ package com.produktiivsusjalgijaklient.klient;
 public class Kasutaja {
     private String kasutajaNimi;
 
+    private String parool;
+
+    public Kasutaja(String kasutajaNimi, String parool) {
+        this.kasutajaNimi = kasutajaNimi;
+        this.parool = parool;
+    }
+
+
 }

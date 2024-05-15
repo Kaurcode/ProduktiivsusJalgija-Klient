1f6b5d6028fca40e981bd86157b4d354d178d4c5
hanneskolk <145332370+hanneskolk@users.noreply.github.com>
2024-05-15 13:37:17 +0300

    UI koos uue kasutaja loomise ja valimisega

--- src/main/java/com/produktiivsusjalgijaklient/klient/MainUI.java
+++ src/main/java/com/produktiivsusjalgijaklient/klient/MainUI.java
@@ -13,7 +13,9 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -35,7 +37,7 @@ public class MainUI extends Application {
 //        } catch (IOException viga) {
 //
 //        }
-        ArrayList<String> andmed = new ArrayList<>(Arrays.asList("valik1", "valik2", "valik3", "valik4", "valik5", "valik6", "valik7"));
+        ArrayList<String> andmed = new ArrayList<>(Arrays.asList("kasutaja", "postgre", "sql", "hannes", "kaur", "kaur2", "kaur3"));
         ObservableList<String> valikudNaidatuna = FXCollections.observableArrayList(andmed);
         peaLava.setScene(kuvaAndmed(valikudNaidatuna));
         peaLava.setTitle("Katsetus");
@@ -61,8 +63,30 @@ public class MainUI extends Application {
 
         valikuVaade.setPadding(new Insets(5));
 
+        Button lookasutajaNupp = new Button("Loo uus kasutaja");
+        lookasutajaNupp.setOnAction(actionEvent -> {
+            try {
+                looUusKasutaja(lookasutajaNupp.getScene(), lookasutajaNupp.getScene().getWindow());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Button valikasutajaNupp = new Button("Vali kasutaja");
+        valikasutajaNupp.setOnAction(actionEvent -> {
+            String valitud = valikuVaade.getSelectionModel().getSelectedItem();
+            if (valitud != null) {
+                valitudKasutaja(valikasutajaNupp.getScene(), valikasutajaNupp.getScene().getWindow(), valitud);
+            } else {
+                System.out.println("Pead valima kasutaja enne edasi minemist");
+            }
+        });
+
         VBox juur = new VBox();
-        juur.getChildren().add(valikuVaade);
+        juur.getChildren().addAll(valikuVaade, lookasutajaNupp, valikasutajaNupp);
+        juur.setAlignment(Pos.CENTER);
+        juur.setSpacing(10);
+        VBox.setMargin(lookasutajaNupp, new Insets(10, 0, 20, 0));
         Scene stseen = new Scene(juur);
         stseen.getStylesheets().add("com/produktiivsusjalgijaklient/klient/Teema.css");
         return stseen;
@@ -144,5 +168,140 @@ public class MainUI extends Application {
         stseen.getStylesheets().add("com/produktiivsusjalgijaklient/klient/Teema.css");
 
         return stseen;
+    }
+
+    private void looUusKasutaja(Scene eelmine, Window omanik) throws IOException {
+        VBox juur = new VBox();
+        juur.setAlignment(Pos.CENTER);
+        juur.setSpacing(10);
+
+        TextField kasutajaNimiInput = new TextField();
+        kasutajaNimiInput.setPromptText("Sisesta kasutajanimi");
+
+        TextField paroolInput = new TextField();
+        paroolInput.setPromptText("Sisesta parool");
+
+        Button looKasutajaNupp = new Button("Loo kasutaja");
+        // Siia saad lisada tegevuse, mis loob uue kasutaja vastavalt sisestatud andmetele
+
+        juur.getChildren().addAll(kasutajaNimiInput, paroolInput, looKasutajaNupp);
+
+        Scene uus = uueKasutajaUI();
+
+        Stage peaLava = (Stage) omanik;
+        peaLava.close();
+
+        Stage uusLava = new Stage();
+        uusLava.setScene(uus);
+        uusLava.initOwner(omanik);
+        uusLava.initModality(Modality.WINDOW_MODAL);
+        uusLava.show();
+    }
+
+    private Scene uueKasutajaUI() throws IOException {
+        VBox juur = new VBox();
+
+        juur.setPadding(new Insets(15));
+        juur.setSpacing(20);
+        juur.setAlignment(Pos.CENTER);
+
+        juur.setPrefHeight(200);
+        juur.setPrefWidth(500);
+
+        Label paiseTekst = new Label("Loo uus kasutaja");
+        juur.getChildren().add(paiseTekst);
+
+        // Ankeet
+
+        GridPane infoSisend = new GridPane();
+        infoSisend.setAlignment(Pos.BASELINE_LEFT);
+        infoSisend.setHgap(20);
+        infoSisend.setVgap(10);
+
+        Label kasutajaNimeSilt = new Label("Uus kasutajanimi:");
+        TextField kasutajaNimeVali = new TextField();
+
+        Label parooliSilt = new Label("Uus parool:");
+        PasswordField parooliVali = new PasswordField();
+
+        Label[] sildid = new Label[] {kasutajaNimeSilt, parooliSilt};
+        TextField[] tekstiValjad = new TextField[] {kasutajaNimeVali, parooliVali};
+
+        final int[] fokuseeritudVali = {0};
+
+        EventHandler<KeyEvent> nupuVajutus = keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                if (fokuseeritudVali[0] == tekstiValjad.length - 1) saadaAndmed(parooliVali.getText());
+                fokuseeritudVali[0]++;
+                fokuseeritudVali[0] = Math.min(fokuseeritudVali[0], tekstiValjad.length - 1);
+                keyEvent.consume();
+            } else if (keyEvent.getCode() == KeyCode.DOWN) {
+                fokuseeritudVali[0]++;
+                fokuseeritudVali[0] = Math.min(fokuseeritudVali[0], tekstiValjad.length - 1);
+                keyEvent.consume();
+            } else if (keyEvent.getCode() == KeyCode.UP) {
+                fokuseeritudVali[0]--;
+                fokuseeritudVali[0] = Math.max(fokuseeritudVali[0], 0);
+                keyEvent.consume();
+            }
+            tekstiValjad[fokuseeritudVali[0]].requestFocus();
+        };
+
+        for (int elemendiNr = 0; elemendiNr < tekstiValjad.length; elemendiNr++) {
+            infoSisend.addRow(elemendiNr, sildid[elemendiNr], tekstiValjad[elemendiNr]);
+            GridPane.setHgrow(tekstiValjad[elemendiNr], Priority.ALWAYS);
+
+            tekstiValjad[elemendiNr].setOnKeyPressed(nupuVajutus);
+
+            final int finalElemendiNr = elemendiNr;
+            tekstiValjad[elemendiNr].focusedProperty().addListener((observableValue, vanaVaartus, uusVaartus) -> {
+                if (uusVaartus) {
+                    fokuseeritudVali[0] = finalElemendiNr;
+                }
+            });
+        }
+
+        juur.getChildren().add(infoSisend);
+
+        Button edasiNupp = new Button("Loo uus kasutaja");
+        juur.getChildren().add(edasiNupp);
+
+        Scene stseen = new Scene(juur);
+        stseen.getStylesheets().add("com/produktiivsusjalgijaklient/klient/Teema.css");
+
+        return stseen;
+    }
+
+    private void valitudKasutaja(Scene eelmine, Window omanik, String valitudKasutaja) {
+        VBox juur = new VBox();
+        juur.setAlignment(Pos.CENTER);
+        juur.setSpacing(10);
+
+        Label kasutajaNimiLabel = new Label("Valitud kasutaja: " + valitudKasutaja);
+
+        GridPane infoSisend = new GridPane();
+        infoSisend.setAlignment(Pos.BASELINE_LEFT);
+        infoSisend.setHgap(20);
+        infoSisend.setVgap(10);
+
+        Label parooliSilt = new Label("Sisesta parool:");
+        PasswordField parooliVali = new PasswordField();
+
+
+        Button logiSisse = new Button("Logi sisse");
+        Button tagasiNupp = new Button("Tagasi");
+
+
+        juur.getChildren().addAll(kasutajaNimiLabel, parooliSilt, parooliVali, logiSisse, tagasiNupp);
+        Scene uusStseen = new Scene(juur, 400, 300);
+        // Sulgeme eelneva stseeni
+        Stage peaLava = (Stage) omanik;
+        peaLava.close();
+
+        Stage uusLava = new Stage();
+        uusLava.setScene(uusStseen);
+        uusLava.initOwner(omanik);
+        uusLava.initModality(Modality.WINDOW_MODAL);
+        uusLava.show();
     }
 }
